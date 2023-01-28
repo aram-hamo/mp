d75c6b756bc23b2bcf5a24bf49c5c655b17b42b7
Aram Hamo <contact@aramhamo.me>
2023-01-28 13:47:56 +0100

    modified:   includes/classes/music.php

--- src/includes/classes/music.php
+++ src/includes/classes/music.php
@@ -5,17 +5,25 @@ class music extends db{
   public function upload($title,$artist,$keywoards,$file_name){
     $fileDetect = new FileDetect;
 
-    $m_stmt = "insert into songs_metadata (title,keywoards) values (:title,:keywoards)";
-    $metadata = $this->conn->prepare($m_stmt);
-    $metadata->bindValue(":title",$title);
-    $metadata->bindValue(":keywoards",$keywoards);
-    $metadata->execute();
-
     $s_stmt = "insert into songs (format,fileName)values(:format,:fileName)";
     $song = $this->conn->prepare($s_stmt);
     $song->bindValue(":format",$fileDetect->mime("content/music/".$file_name));
     $song->bindValue(":fileName",$file_name);
     $song->execute();
+
+    // getting the song id
+    $getId = $this->conn->prepare("select id from songs where fileName=:fileName");
+    $getId->bindValue(':fileName',$file_name);
+    $getId->execute();
+    $Id =  $getId->fetchAll();
+
+    $m_stmt = "insert into songs_metadata (title,keywoards,sId) values (:title,:keywoards,:sId)";
+    $metadata = $this->conn->prepare($m_stmt);
+    $metadata->bindValue(":title",$title);
+    $metadata->bindValue(":keywoards",$keywoards);
+    $metadata->bindValue(":sId",$Id[0]["id"]);
+    $metadata->execute();
+
   }
   public function addArtist($firstName,$lastName,$nickName,$description){
     $astmt = "insert into artists (firstName,lastName,nickName,description) values (:firstName,:lastName,:nickName,:description)";
