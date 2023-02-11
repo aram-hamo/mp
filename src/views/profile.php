8445a3c575b3f62fbe64c6696f5460823547522f
Aram Hamo <contact@aramhamo.me>
2023-02-11 11:46:09 +0100

    modified:   views/profile.php

--- src/views/profile.php
+++ src/views/profile.php
@@ -1,11 +1,42 @@
 <?php include('includes/header.php'); ?>
-<?php include('includes/navbar.php'); ?>
 <?php
 $auth = new auth;
-$userID = $auth->getUserDataByToken($_COOKIE['tokan'])[0]['id'];
-$music = new music;
 if(!$auth->tokanCheck()){
+  header("Location: /");
   die();
 }
 ?>
+<?php include('includes/navbar.php'); ?>
+<?php
+$userID = $auth->getUserDataByToken($_COOKIE['tokan'])[0]['id'];
+$music = new music;
+?>
+<form method="post">
+<br>
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="inputGroup-sizing-default">Username</span>
+  </div>
+  <input value="<?=$userData[0]['username']?>" type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+</div>
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="inputGroup-sizing-default">First Name</span>
+  </div>
+  <input value="<?=$userData[0]['firstName']?>" type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+</div>
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="inputGroup-sizing-default">Last Name</span>
+  </div>
+  <input value="<?=$userData[0]['lastName']?>" type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+</div>
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="inputGroup-sizing-default">E-Mail</span>
+  </div>
+  <input value="<?=$userData[0]['email']?>" type="email" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+</div>
+<input class="btn btn-primary" type="submit" value="Update">
+</form>
 <?php include('includes/footer.php'); ?>
