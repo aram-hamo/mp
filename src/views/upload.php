07cfdd1edfd33ab9709c6911d3df8859d753cf03
Aram Hamo <contact@aramhamo.me>
2023-02-13 17:55:01 +0100

    modified:   views/upload.php

--- src/views/upload.php
+++ src/views/upload.php
@@ -9,18 +9,21 @@ if(!$auth->tokanCheck()){
 }
 ?>
 <form class="form-group" method="post" enctype="multipart/form-data">
-  <input class="form-control" name="file" type="file">
+  <input class="form-control" name="songs[]" multiple=multiple type="file">
   <input class="form-control btn btn-primary " name="submit" type="submit" value="upload">
 </form>
 <?php
 if(isset($_POST['submit'])){
-  $fileName = bin2hex(openssl_random_pseudo_bytes(16));
-  $file = file_get_contents($_FILES['file']['tmp_name']);
-  move_uploaded_file($_FILES['file']['tmp_name'], 'content/music/'.$fileName);
+  $songsCount = count($_FILES['songs']['name']);
+  $songs = count($_FILES['songs']['name']);
+  for($i = 0;$i < $songsCount ;$i++){
+    $fileName = bin2hex(openssl_random_pseudo_bytes(16));
+    $file = file_get_contents($_FILES['songs']['tmp_name'][$i]);
+    move_uploaded_file($_FILES['songs']['tmp_name'][$i], 'content/music/'.$fileName);
 
-  $song = new music;
-  $song->upload($fileName);
-  header("Location: /dashboard/edit-metadata?songID=$fileName");
+    $song = new music;
+    $song->upload($fileName);
+  }
 }
 ?>
 <?php include('includes/footer.php'); ?>
