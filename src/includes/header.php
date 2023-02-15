3e67e7117731838f0f1018b8de93cd80e8324e02
Aram Hamo <contact@aramhamo.me>
2023-02-15 07:36:40 +0100

    modified:   src/includes/header.php 	modified:   src/static/manifest.json

--- src/includes/header.php
+++ src/includes/header.php
@@ -4,6 +4,7 @@
     <link rel=stylesheet href="<?=rootURL?>/static/bootstrap/css/bootstrap.min.css" >
     <link rel=stylesheet href="<?=rootURL?>/includes/style.css" >
     <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name=theme-color content="#F8F9FA">
     <link rel=manifest  href=static/manifest.json>
 </head>
 <body>
