4da4754ed0127fc1d10853749e85974dff64d078
Aram Hamo <contact@aramhamo.me>
2023-03-05 17:28:54 +0100

    modified:   includes/footer.php 	modified:   includes/navbar.php 	new file:   static/bootstrap/js/bootstrap.min.js 	new file:   static/jquery-3.3.1.slim.min.js

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,3 +1,5 @@
 <script src="<?=rootURL?>/pwa.js" ></script>
+<script src="<?=rootURL?>/static/jquery-3.3.1.slim.min.js"></script>
+<script src="<?=rootURL?>/static/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
--- src/includes/navbar.php
+++ src/includes/navbar.php
@@ -2,29 +2,31 @@
 $auth = new auth;
 $userData = $auth->getUserDataByToken($_COOKIE['tokan']);
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">MP</a>
-
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/dashboard/playlists">Playlists</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/dashboard/upload">Upload</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/dashboard/allsongs">My Songs</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/dashboard/profile"><?=$userData[0]['firstName'],' ',$userData[0]['lastName'],'( '.$userData[0]['username'].' )' ?></a>
-      </li>
-      <li class="nav-item">
-        <form method="post">
-          <input name="logout" class="btn " value="Logout" type="submit">
-        </form>
-      </li>
-    </ul>
-
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/dashboard">MP</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/dashboard/playlists">Playlists</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/dashboard/upload">Upload</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/dashboard/allsongs">My Songs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/dashboard/profile"><?=$userData[0]['firstName'],' ',$userData[0]['lastName'],'( '.$userData[0]['username'].' )' ?></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">Logout</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
 <?php
